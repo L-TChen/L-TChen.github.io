924205ec777e4b3a94ba2cf3037d6cedb28b74fc
Liang-Ting Chen <liang.ting.chen.tw@gmail.com>
2020-11-13 15:18:31 +0800

    New index page

---
+++ content/index.md
@@ -0,0 +1,31 @@
+---
+  title: Liang-Ting Chen
+---
+
+About
+-----
+__Chen, Liang-Ting 陳亮廷__  
+Postdoctoral researcher  
+[Institute of Information Science](https://www.iis.sinica.edu.tw), [Academia Sinica](https://www.sinica.edu.tw/)  
+Room N602, IIS, 128 Academia Road, Section 2, Taipei 115201, Taiwan
+
+Interests
+---------
+
+My research interests include 
+
+  * automata theory
+  * modal logic
+  * category theory
+  * type theory
+  * dependently typed programming and formalisation
+
+Roles
+-----
+
+Manuscripts
+-----------
+
+Publications
+------------
+
--- src/site.hs
+++ src/site.hs
@@ -6,66 +6,35 @@ import           Hakyll.Web.Pandoc.Biblio
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "assets/images/*" $ do
-        route $ gsubRoute "assets/" (const "")
-        compile copyFileCompiler
+  match "assets/images/*" $ do
+      route $ gsubRoute "assets/" (const "")
+      compile copyFileCompiler
 
-    match "assets/css/*" $ do
-        route $ gsubRoute "assets/" (const "")
-        compile compressCssCompiler
+  match "assets/css/*" $ do
+      route $ gsubRoute "assets/" (const "")
+      compile compressCssCompiler
 
-    match "assets/bib/*" $ compile biblioCompiler
-    match "assets/csl/*" $ compile cslCompiler
+  match "assets/bib/*" $ compile biblioCompiler
+  match "assets/csl/*" $ compile cslCompiler
 
-    match "content/cv.md" $ do
-        route   $ gsubRoute "content/" (const "") `composeRoutes` setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match "content/publications.md" $ do
+      route   $ gsubRoute "content/" (const "") `composeRoutes` setExtension "html"
+      compile $ pandocBiblioCompiler "assets/csl/elsevier-with-titles.csl" "assets/bib/published.bib"
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= loadAndApplyTemplate "templates/navbar.html" defaultContext
+        >>= loadAndApplyTemplate "templates/head.html"  defaultContext
+        >>= relativizeUrls
 
-    match "content/publications.md" $ do
-        route   $ gsubRoute "content/" (const "") `composeRoutes` setExtension "html"
-        compile $ pandocBiblioCompiler "assets/csl/elsevier-with-titles.csl" "assets/bib/published.bib"
-          >>= loadAndApplyTemplate "templates/default.html" defaultContext
-          >>= relativizeUrls
-{-
-    match "content/posts/*" $ do
-        route $ gsubRoute "content/" (const "/")
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-    create ["posts.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Posts"               `mappend`
-                    defaultContext
+  match "content/**.md" $ do
+      route $ gsubRoute "content/" (const "") `composeRoutes` setExtension "html"
+      let indexCtx = defaultContext
+      compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= loadAndApplyTemplate "templates/navbar.html" indexCtx
+        >>= loadAndApplyTemplate "templates/head.html" indexCtx
+        >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/posts.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
--}
-
-    match "content/index.html" $ do
-        route $ gsubRoute "content/" (const "")
-        compile $ do
-            --posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-            --        listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "About"               `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateBodyCompiler
+  match "templates/*" $ compile templateBodyCompiler
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
