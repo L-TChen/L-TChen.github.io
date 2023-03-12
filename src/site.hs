7069256c90a9c4dd644b4048f7c3bedd6f872de8
Liang-Ting Chen <liang.ting.chen.tw@gmail.com>
2023-03-13 03:22:39 +0800

    Use date to create a corresponding URL

--- src/site.hs
+++ src/site.hs
@@ -1,74 +1,107 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll hiding (pandocBiblioCompiler)
 
-import           Control.Monad                 (liftM)
-import           Text.Pandoc                   (Extension (..), Pandoc,
-                                                ReaderOptions (..),
-                                                WriterOptions (..),
-                                                enableExtension,
-                                                writerHTMLMathMethod,
-                                                HTMLMathMethod(..))
+import Control.Monad (liftM)
+import Data.List (intercalate)
+import Data.Maybe (fromJust)
+import Data.Monoid (mappend)
+import Data.Time.Format
+import Hakyll hiding (pandocBiblioCompiler)
+import System.FilePath
+  ( dropExtension,
+    joinPath,
+    splitDirectories,
+    splitPath,
+    takeBaseName,
+    takeDirectory,
+  )
+import Text.Pandoc
+  ( Extension (..),
+    HTMLMathMethod (..),
+    Pandoc,
+    ReaderOptions (..),
+    WriterOptions (..),
+    enableExtension,
+    writerHTMLMathMethod,
+  )
+
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
   match "assets/img/*" $ do
-      route $ gsubRoute "assets/" (const "")
-      compile copyFileCompiler
+    route $ gsubRoute "assets/" (const "")
+    compile copyFileCompiler
 
   match "assets/css/*" $ do
-      route $ gsubRoute "assets/" (const "")
-      compile compressCssCompiler
+    route $ gsubRoute "assets/" (const "")
+    compile compressCssCompiler
 
   match "assets/bib/*" $ compile biblioCompiler
   match "assets/csl/*" $ compile cslCompiler
 
+  match "templates/*" $ compile templateBodyCompiler
+
   match "content/*.md" $ do
-      route $ gsubRoute "content/" (const "") `composeRoutes` setExtension "html"
-      let indexCtx = defaultContext
-      compile $ pandocBiblioCompiler "assets/csl/elsevier-with-titles.csl" "assets/bib/*.bib"
+    route $ gsubRoute "content/" (const "") `composeRoutes` setExtension "html"
+    let indexCtx = defaultContext
+    compile $
+      pandocBiblioCompiler "assets/csl/elsevier-with-titles.csl" "assets/bib/*.bib"
         >>= loadAndApplyTemplate "templates/default.html" indexCtx
         >>= loadAndApplyTemplate "templates/navbar.html" indexCtx
         >>= loadAndApplyTemplate "templates/head.html" indexCtx
         >>= relativizeUrls
 
   match "content/posts/**.md" $ do
-      route $ gsubRoute "content/" (const "") `composeRoutes` setExtension "html"
+    route $
+      postURL `composeRoutes` setExtension "html"
 
-      let indexCtx = postCtx
-      compile $ pandocBiblioCompiler "assets/csl/elsevier-with-titles.csl" "assets/bib/*.bib"
-        >>= loadAndApplyTemplate "templates/post.html"   indexCtx
+    let indexCtx = postCtx
+    compile $
+      pandocBiblioCompiler "assets/csl/elsevier-with-titles.csl" "assets/bib/*.bib"
+        >>= loadAndApplyTemplate "templates/post.html" indexCtx
         >>= loadAndApplyTemplate "templates/navbar.html" indexCtx
-        >>= loadAndApplyTemplate "templates/head.html"   indexCtx
+        >>= loadAndApplyTemplate "templates/head.html" indexCtx
         >>= relativizeUrls
-
-  match "templates/*" $ compile templateBodyCompiler
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  dateField "date" "%B %e, %Y" `mappend`
+  defaultContext `mappend`
+  field "category" (\it -> do
+    let paths = drop 2 $ splitPath $ takeDirectory $ toFilePath $ itemIdentifier it
+    if null paths
+      then noResult "no category name is found"
+      else return (joinPath paths))
+
+postURL :: Routes
+postURL = customRoute $ \id' ->
+  let base = takeBaseName $ toFilePath id'
+      (date, title) = splitAt 3 $ splitAll "-" base
+   in joinPath $ "posts" : date ++ [intercalate "-" title]
+
+--------------------------------------------------------------------------------
+ropt :: ReaderOptions
+ropt =
+  defaultHakyllReaderOptions
+    { -- The following option enables citation rendering
+      readerExtensions =
+        enableExtension Ext_superscript $
+          enableExtension Ext_subscript $
+            enableExtension Ext_citations $
+              enableExtension Ext_grid_tables $
+                readerExtensions defaultHakyllReaderOptions
+    }
+
+wopt :: WriterOptions
+wopt =
+  defaultHakyllWriterOptions
+    { writerHTMLMathMethod = MathJax ""
+    }
 
 pandocBiblioCompiler :: String -> String -> Compiler (Item String)
 pandocBiblioCompiler cslFileName bibFileName = do
   csl <- load $ fromFilePath cslFileName
   bibs <- loadAll $ fromGlob bibFileName
-  liftM (writePandocWith wopt)
-    (getResourceBody >>= readPandocBiblios ropt csl bibs)
-  where
-    ropt = defaultHakyllReaderOptions
-      { -- The following option enables citation rendering
-        readerExtensions =
-          enableExtension Ext_superscript $
-          enableExtension Ext_subscript $
-          enableExtension Ext_citations $
-          enableExtension Ext_grid_tables $
-          readerExtensions defaultHakyllReaderOptions
-      }
-    wopt = defaultHakyllWriterOptions
-      {
-        writerHTMLMathMethod = MathJax "" 
-      }
-  
+  writePandocWith wopt
+    <$> (getResourceBody >>= readPandocBiblios ropt csl bibs)
